4e11a3bd34a4d4aa135f5dd20ca348843785ed1a
unknown <victorgab0802@hmail.com>
2024-10-10 07:24:22 -0300

    iniciei o sistema de login

--- app.py
+++ app.py
@@ -4,16 +4,26 @@ from flask_sqlalchemy import SQLAlchemy
 from database.db import db
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
+#conectando com o banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.register_blueprint(app_route)
+lm = LoginManager(app)
+app.secret_key= "qwe123"
 
+@lm.user_loader
+def save_user(app):
+    
+
+#inicializando o banco de dados
 db.init_app(app)
   
 @app.route("/")
 def iniciar():
     return render_template('index.html')
 
+#criando o banco de dados
 with app.app_context():
     db.create_all()
 app.run(debug= True)    
